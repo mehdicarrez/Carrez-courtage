d93681d1224c3cd5033ff2afe86e22018bec23df
mehdi <m.eljahid@2ca-media.com>
2026-09-21 08:29:25 +0000

    logo-commits

--- extranet-carrez/resources/js/pages/Login.jsx
+++ extranet-carrez/resources/js/pages/Login.jsx
@@ -285,8 +285,8 @@ export default function Login() {
                     </div>
 
                     <div className="relative flex items-center gap-3">
-                        <span className="inline-flex h-14 w-14 items-center justify-center rounded-2xl bg-white shadow-lg">
-                            <img src="/images/logo-carrez.png" alt="Carrez Co Courtage" className="h-90 w-90 object-contain" />
+                        <span className="inline-flex h-28 w-28 items-center justify-center rounded-2xl bg-white shadow-lg">
+                            <img src="/images/logo-carrez.png" alt="Carrez Co Courtage" className="h-24 w-24 object-contain" />
                         </span>
                         <div>
                             {/* <div className="text-lg font-bold tracking-tight">Carrez Co Courtage</div> */}
@@ -355,8 +355,8 @@ export default function Login() {
                 </div>
 
                 <div className="relative flex items-center gap-3">
-                    <span className="inline-flex h-14 w-14 items-center justify-center rounded-2xl bg-white shadow-lg">
-                        <img src="/images/logo-carrez.png" alt="Carrez Co Courtage" className="h-9 w-9 object-contain" />
+                    <span className="inline-flex h-28 w-28 items-center justify-center rounded-2xl bg-white shadow-lg">
+                        <img src="/images/logo-carrez.png" alt="Carrez Co Courtage" className="h-24 w-24 object-contain" />
                     </span>
                     <div>
                         <div className="text-lg font-bold tracking-tight">Carrez Co Courtage</div>
